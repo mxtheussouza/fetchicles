276e5bda873a0f4ca3085763e65b89a78ec30beb
matheusdearaujo <matheusouzadearaujo@gmail.com>
2022-10-04 22:22:33 -0300

    no message

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-export const hello = () => console.log("Hello, world!");
+import fetch from "node-fetch";
 
-hello();
+export const brands = async (vehicle: string) => {
+	const url = `http://127.0.0.1:5151/api/brands/${vehicle}`;
+
+	const response = await fetch(url);
+
+	const data = await response.json();
+
+	return data;
+};
+
+console.log(brands("car"));
