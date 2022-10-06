3de9c493207d74cbad4ffa641bb699067902fece
Anthony Victor <thonnyvrc@gmail.com>
2022-10-06 10:41:34 -0300

    feat:changed node-fetch to axios

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-import fetch from "node-fetch";
+import axios from "axios";
 
 export const brands = async (vehicle: string) => {
 	const url = `http://127.0.0.1:5151/api/brands/${vehicle}`;
 
-	const response = await fetch(url);
+	const response = await axios.get(url);
 
-	const data = await response.json();
-
-	return data;
+	return response.data;
 };
 
-console.log(brands("car"));
+brands("car").then(data => console.log(data));
