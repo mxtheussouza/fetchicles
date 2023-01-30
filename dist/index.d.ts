de755837b2437b4e9444da13268b7b2db914dadc
matheusdearaujo <matheusouzadearaujo@gmail.com>
2023-01-30 11:59:07 -0300

    fix: remove dist folder

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare type Vehicles = "car" | "motorcycle";
-declare type Models = {
-	vehicle: Vehicles;
-	brand: string;
-};
-export declare const brands: (vehicle: Vehicles) => Promise<string[]>;
-export declare const models: ({ vehicle, brand }: Models) => Promise<string[]>;
-export {};
